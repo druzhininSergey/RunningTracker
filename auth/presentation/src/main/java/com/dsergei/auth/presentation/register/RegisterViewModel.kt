106f19db6b21e5ba5b70239ce493dd5f2997e875
sergeydruzhinin <druzhininsergey96@gmail.com>
2025-02-17 12:02:05 +0700

    3.3 Validating user data

--- auth/presentation/src/main/java/com/dsergei/auth/presentation/register/RegisterViewModel.kt
+++ auth/presentation/src/main/java/com/dsergei/auth/presentation/register/RegisterViewModel.kt
@@ -4,12 +4,43 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dsergei.auth.domain.UserDataValidator
+import com.dsergei.core.presentation.ui.textAsFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
-class RegisterViewModel : ViewModel() {
+class RegisterViewModel(
+    private val userDataValidator: UserDataValidator
+) : ViewModel() {
+
     var state by mutableStateOf(RegisterState())
         private set
 
-    fun onAction(action: RegisterAction) {
+    init {
+        state.email.textAsFlow()
+            .onEach { email ->
+                val isValidEmail = userDataValidator.isValidEmail(email.toString())
+                state = state.copy(
+                    isEmailValid = isValidEmail,
+                    canRegister = isValidEmail && state.passwordValidationState.isValidPassword
+                            && !state.isRegistering
+                )
+            }
+            .launchIn(viewModelScope)
 
+        state.password.textAsFlow()
+            .onEach { password ->
+                val passwordValidationState =
+                    userDataValidator.validatePassword(password.toString())
+                state = state.copy(
+                    passwordValidationState = passwordValidationState,
+                    canRegister = state.isEmailValid && passwordValidationState.isValidPassword
+                            && !state.isRegistering
+                )
+            }
+            .launchIn(viewModelScope)
     }
+
+    fun onAction(action: RegisterAction) {}
 }
