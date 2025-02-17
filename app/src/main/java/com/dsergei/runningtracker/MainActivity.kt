59c33dd4aa38a19c965f79d2cc0884b03f583432
sergeydruzhinin <druzhininsergey96@gmail.com>
2025-02-17 21:40:52 +0700

    3.4 Navigation graph & DI setup

--- app/src/main/java/com/dsergei/runningtracker/MainActivity.kt
+++ app/src/main/java/com/dsergei/runningtracker/MainActivity.kt
@@ -5,12 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.dsergei.core.presentation.desingsystem.AnalyticsIcon
-import com.dsergei.runningtracker.ui.theme.RunningTrackerTheme
+import androidx.navigation.compose.rememberNavController
+import com.dsergei.core.presentation.desingsystem.RunningTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +21,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Icon(AnalyticsIcon, null)
+                    NavigationRoot(
+                        navController = rememberNavController(),
+                        isLoggedIn = false,
+                        {}
+                    )
                 }
             }
         }
---
+++ app/src/main/java/com/dsergei/runningtracker/di/AppModule.kt
@@ -0,0 +1,7 @@
+package com.dsergei.runningtracker.di
+
+import org.koin.dsl.module
+
+val appModule = module {
+
+}
